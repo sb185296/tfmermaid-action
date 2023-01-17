9447700a5c5053dcdd40f79e4adc83c13f9c0f9e
shay ben tikva <66299621+sb185296@users.noreply.github.com>
2023-01-17 20:20:10 +0200

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -2,6 +2,6 @@
 
 temp=$(mktemp $1.XXXXXXXXXX)
 
-terraform init -backend=false
+terraform init -reconfigure -backend=false
 terraform graph | node $(dirname $0)/index.mjs $1 $2 > $temp
 mv $temp $1
